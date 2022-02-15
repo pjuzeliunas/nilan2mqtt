117066544ce9b58bec94d1dc46d566eef86eed6a
Povilas Juzeliunas <pjuzeliunas@gmail.com>
2022-02-15 20:37:20 +0100

    Adapter logic moved to a separate struct

--- cmd/nilan2mqtt/main.go
+++ cmd/nilan2mqtt/main.go
@@ -1,167 +1,20 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
-	"time"
 
-	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"github.com/google/uuid"
-	"github.com/pjuzeliunas/nilan"
-	"github.com/pjuzeliunas/nilan2mqtt/internal/config"
-	"github.com/pjuzeliunas/nilan2mqtt/internal/dto"
+	"github.com/pjuzeliunas/nilan2mqtt/internal/adapter"
 )
 
-func mqttClient(brokerAddress string, port int, username string, password string) mqtt.Client {
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", brokerAddress, port))
-	opts.SetClientID(uuid.New().String())
-	opts.SetUsername(username)
-	opts.SetPassword(password)
-	opts.OnConnectionLost = func(client mqtt.Client, err error) {
-		panic(err)
-	}
-	opts.SetDefaultPublishHandler(processMessage)
-	c := mqtt.NewClient(opts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-	return c
-}
-
-func subscribeForTopics(client mqtt.Client) {
-	topics := []string{
-		"homeassistant/fan/nilan/set",
-		"homeassistant/fan/nilan/speed/set",
-		"homeassistant/fan/nilan/mode/set",
-	}
-	for _, t := range topics {
-		token := client.Subscribe(t, 1, nil)
-		token.Wait()
-	}
-}
-
-func processMessage(client mqtt.Client, msg mqtt.Message) {
-	payload := string(msg.Payload())
-	switch msg.Topic() {
-	case "homeassistant/fan/nilan/set":
-		settings := nilan.Settings{
-			VentilationOnPause: boolAddr(payload == "OFF"),
-		}
-		NilanController.SendSettings(settings)
-	case "homeassistant/fan/nilan/speed/set":
-		speed, _ := strconv.Atoi(payload)
-		settings := nilan.Settings{
-			FanSpeed: dto.FanSpeed(speed),
-		}
-		NilanController.SendSettings(settings)
-	case "homeassistant/fan/nilan/mode/set":
-		settings := nilan.Settings{
-			VentilationMode: dto.Mode(payload),
-		}
-		NilanController.SendSettings(settings)
-	}
-	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
-}
-
-func boolAddr(b bool) *bool {
-	boolVar := b
-	return &boolVar
-}
-
-func sendSimpleConfig(client mqtt.Client, topic string, config config.SimpleConfig) {
-	d, _ := json.Marshal(config)
-	t := client.Publish(topic, 0, false, d)
-	t.Wait()
-}
-
-func sendFanConfig(client mqtt.Client, topic string, config config.Fan) {
-	d, _ := json.Marshal(config)
-	t := client.Publish(topic, 0, false, d)
-	t.Wait()
-}
-
-func setUpConfig(client mqtt.Client) {
-	sendSimpleConfig(client, "homeassistant/sensor/nilan/1/config", config.RoomTemperature())
-	sendSimpleConfig(client, "homeassistant/sensor/nilan/2/config", config.OutdoorTemperature())
-	sendSimpleConfig(client, "homeassistant/sensor/nilan/3/config", config.HumidityAvg())
-	sendSimpleConfig(client, "homeassistant/sensor/nilan/4/config", config.Humidity())
-	sendSimpleConfig(client, "homeassistant/sensor/nilan/5/config", config.DHWTemperatureTop())
-	sendSimpleConfig(client, "homeassistant/sensor/nilan/6/config", config.DHWTemperatureBottom())
-	sendSimpleConfig(client, "homeassistant/sensor/nilan/7/config", config.SupplyFlowTemperature())
-	sendFanConfig(client, "homeassistant/fan/nilan/config", config.NilanVentilation())
-}
-
-func publishReadings(client mqtt.Client, readings dto.Readings) {
-	d, _ := json.Marshal(readings)
-	t := client.Publish("homeassistant/sensor/nilan/state", 0, false, d)
-	t.Wait()
-}
-
-func publishVentilationState(client mqtt.Client, ventilationState dto.Ventilation) {
-	d, _ := json.Marshal(ventilationState)
-	t := client.Publish("homeassistant/fan/nilan/state", 0, false, d)
-	t.Wait()
-}
-
-func startFetchingReadings(controller nilan.Controller, readingsChan chan nilan.Readings) {
-	for {
-		readings, err := controller.FetchReadings()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		readingsChan <- *readings
-		time.Sleep(time.Minute)
-	}
-}
-
-func startFetchingSettings(controller nilan.Controller, settingsChan chan nilan.Settings) {
-	for {
-		settings, err := controller.FetchSettings()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		settingsChan <- *settings
-		time.Sleep(time.Minute)
-	}
-}
-
-func publishReadingsFromChan(readingsChan chan nilan.Readings, client mqtt.Client) {
-	for readings := range readingsChan {
-		readingsDTO := dto.CreateReadingsDTO(readings)
-		publishReadings(client, readingsDTO)
-	}
-}
-
-func publishSettingsFromChan(settingsChan chan nilan.Settings, client mqtt.Client) {
-	for settings := range settingsChan {
-		ventilationDTO := dto.CreateVentilationDTO(settings)
-		publishVentilationState(client, ventilationDTO)
-	}
-}
-
-var NilanController nilan.Controller = nilan.Controller{Config: nilan.Config{NilanAddress: "192.168.1.31:502"}}
-
 func main() {
-	mqttC := mqttClient("192.168.1.18", 1883, "", "")
-	defer mqttC.Disconnect(0)
-
-	setUpConfig(mqttC)
-	subscribeForTopics(mqttC)
-
-	readingsChan := make(chan nilan.Readings)
-	go startFetchingReadings(NilanController, readingsChan)
-	go publishReadingsFromChan(readingsChan, mqttC)
-
-	settingsChan := make(chan nilan.Settings)
-	go startFetchingSettings(NilanController, settingsChan)
-	go publishSettingsFromChan(settingsChan, mqttC)
+	adapter := adapter.NewNilanMQTTAdapter(
+		"192.168.1.31:502",  // Nilan address
+		"192.168.1.18:1883", // MQTT broker address
+		"", "",
+	)
+	adapter.Start()
 
 	quitChannel := make(chan os.Signal, 1)
 	signal.Notify(quitChannel, syscall.SIGINT, syscall.SIGTERM)
---
+++ internal/adapter/nilan-mqtt.go
@@ -0,0 +1,210 @@
+package adapter
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/google/uuid"
+	"github.com/pjuzeliunas/nilan"
+	"github.com/pjuzeliunas/nilan2mqtt/internal/config"
+	"github.com/pjuzeliunas/nilan2mqtt/internal/dto"
+)
+
+type NilanMQTTAdapter struct {
+	nilanController nilan.Controller
+	mqttClient      mqtt.Client
+
+	readingsChan chan nilan.Readings
+	settingsChan chan nilan.Settings
+
+	running bool
+}
+
+func NewNilanMQTTAdapter(nilanAddress string, mqttBrokerAddress string, mqttUsername string, mqttPassword string) NilanMQTTAdapter {
+	a := NilanMQTTAdapter{}
+
+	a.setUpController(nilanAddress)
+	a.setUpMQTTClient(mqttBrokerAddress, mqttUsername, mqttPassword)
+	a.running = false
+
+	return a
+}
+
+func (a *NilanMQTTAdapter) Start() {
+	a.running = true
+	a.sendConfig()
+
+	a.readingsChan = make(chan nilan.Readings)
+	go a.startFetchingReadings()
+	go a.startPublishingReadings()
+
+	a.settingsChan = make(chan nilan.Settings)
+	go a.startFetchingSettings()
+	go a.startPublishingSettings()
+
+	a.subscribeForTopics()
+}
+
+func (a *NilanMQTTAdapter) Stop() {
+	a.running = false
+}
+
+func (a *NilanMQTTAdapter) setUpController(address string) {
+	a.nilanController = nilan.Controller{Config: nilan.Config{NilanAddress: address}}
+}
+
+func (a *NilanMQTTAdapter) setUpMQTTClient(address string, username string, password string) {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s", address))
+	opts.SetClientID(uuid.New().String())
+	opts.SetUsername(username)
+	opts.SetPassword(password)
+	opts.OnConnectionLost = a.reconnect
+
+	a.mqttClient = mqtt.NewClient(opts)
+	a.tryConnectToMQTT(0)
+}
+
+func (a *NilanMQTTAdapter) subscribeForTopics() {
+	topics := []string{
+		"homeassistant/fan/nilan/set",
+		"homeassistant/fan/nilan/speed/set",
+		"homeassistant/fan/nilan/mode/set",
+	}
+	for _, t := range topics {
+		token := a.mqttClient.Subscribe(t, 1, a.processMessage)
+		token.Wait()
+	}
+}
+
+func (a *NilanMQTTAdapter) processMessage(client mqtt.Client, msg mqtt.Message) {
+	payload := string(msg.Payload())
+	switch msg.Topic() {
+	case "homeassistant/fan/nilan/set":
+		settings := nilan.Settings{
+			VentilationOnPause: boolAddr(payload == "OFF"),
+		}
+		a.nilanController.SendSettings(settings)
+		a.fetchSettings()
+	case "homeassistant/fan/nilan/speed/set":
+		speed, _ := strconv.Atoi(payload)
+		settings := nilan.Settings{
+			FanSpeed: dto.FanSpeed(speed),
+		}
+		a.nilanController.SendSettings(settings)
+		a.fetchSettings()
+	case "homeassistant/fan/nilan/mode/set":
+		settings := nilan.Settings{
+			VentilationMode: dto.Mode(payload),
+		}
+		a.nilanController.SendSettings(settings)
+		a.fetchSettings()
+	}
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+func boolAddr(b bool) *bool {
+	boolVar := b
+	return &boolVar
+}
+
+func (a *NilanMQTTAdapter) reconnect(client mqtt.Client, err error) {
+	a.tryConnectToMQTT(3)
+}
+
+func (a *NilanMQTTAdapter) tryConnectToMQTT(attempts int) {
+	if token := a.mqttClient.Connect(); token.Wait() && token.Error() != nil {
+		if attempts > 0 {
+			time.Sleep(time.Second * 10)
+			a.tryConnectToMQTT(attempts - 1)
+		} else {
+			panic(token.Error())
+		}
+	}
+}
+
+func (a *NilanMQTTAdapter) sendSimpleConfig(topic string, config config.SimpleConfig) {
+	d, _ := json.Marshal(config)
+	t := a.mqttClient.Publish(topic, 0, false, d)
+	t.Wait()
+}
+
+func (a *NilanMQTTAdapter) sendFanConfig(topic string, config config.Fan) {
+	d, _ := json.Marshal(config)
+	t := a.mqttClient.Publish(topic, 0, false, d)
+	t.Wait()
+}
+
+func (a *NilanMQTTAdapter) sendConfig() {
+	a.sendSimpleConfig("homeassistant/sensor/nilan/1/config", config.RoomTemperature())
+	a.sendSimpleConfig("homeassistant/sensor/nilan/2/config", config.OutdoorTemperature())
+	a.sendSimpleConfig("homeassistant/sensor/nilan/3/config", config.HumidityAvg())
+	a.sendSimpleConfig("homeassistant/sensor/nilan/4/config", config.Humidity())
+	a.sendSimpleConfig("homeassistant/sensor/nilan/5/config", config.DHWTemperatureTop())
+	a.sendSimpleConfig("homeassistant/sensor/nilan/6/config", config.DHWTemperatureBottom())
+	a.sendSimpleConfig("homeassistant/sensor/nilan/7/config", config.SupplyFlowTemperature())
+	a.sendFanConfig("homeassistant/fan/nilan/config", config.NilanVentilation())
+}
+
+func (a *NilanMQTTAdapter) startFetchingReadings() {
+	for a.running {
+		a.fetchReadings()
+		time.Sleep(time.Second * 5)
+	}
+	close(a.readingsChan)
+}
+
+func (a *NilanMQTTAdapter) fetchReadings() {
+	readings, err := a.nilanController.FetchReadings()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	a.readingsChan <- *readings
+}
+
+func (a *NilanMQTTAdapter) startFetchingSettings() {
+	for a.running {
+		a.fetchSettings()
+		time.Sleep(time.Second * 5)
+	}
+	close(a.settingsChan)
+}
+
+func (a *NilanMQTTAdapter) fetchSettings() {
+	settings, err := a.nilanController.FetchSettings()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	a.settingsChan <- *settings
+}
+
+func (a *NilanMQTTAdapter) startPublishingReadings() {
+	for readings := range a.readingsChan {
+		readingsDTO := dto.CreateReadingsDTO(readings)
+		a.publishReadings(readingsDTO)
+	}
+}
+
+func (a *NilanMQTTAdapter) publishReadings(readings dto.Readings) {
+	d, _ := json.Marshal(readings)
+	t := a.mqttClient.Publish("homeassistant/sensor/nilan/state", 0, false, d)
+	t.Wait()
+}
+
+func (a *NilanMQTTAdapter) startPublishingSettings() {
+	for settings := range a.settingsChan {
+		ventilationDTO := dto.CreateVentilationDTO(settings)
+		a.publishVentilationState(ventilationDTO)
+	}
+}
+
+func (a *NilanMQTTAdapter) publishVentilationState(ventilationState dto.Ventilation) {
+	d, _ := json.Marshal(ventilationState)
+	t := a.mqttClient.Publish("homeassistant/fan/nilan/state", 0, false, d)
+	t.Wait()
+}
